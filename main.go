62d6e37d0b1087e26d1526439d5822c3faa1c160
zhouli <mfktqti_zhou@163.com>
2022-11-18 19:59:01 +0800

    执行一次请求，就开启一个浏览器

--- main.go
+++ main.go
@@ -13,18 +13,6 @@ import (
 
 func main() {
 	// 禁用chrome headless
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", false),
-	)
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
-
-	// create chrome instance
-	ctx, cancel := chromedp.NewContext(
-		allocCtx,
-		chromedp.WithLogf(log.Printf),
-	)
-	defer cancel()
 
 	f, err := excelize.OpenFile("config.xlsx")
 	if err != nil {
@@ -44,9 +32,41 @@ func main() {
 		return
 	}
 
+	for i := 0; i < len(rows); i++ {
+		cells := rows[i]
+		username := cells[0]
+		pass := cells[1]
+		runChromedp(username, pass)
+	}
+}
+
+func runChromedp(username, password string) {
 	var status, points string
-	username := "adeshaye@hotmail.com"
-	pass := "pm1maki"
+
+	// // create chrome instance
+	// ctx, cancel := chromedp.NewContext(
+	// 	context.Background(),
+	// 	// chromedp.WithDebugf(log.Printf),
+	// )
+	// defer cancel()
+
+	// // create a timeout
+	// ctx, cancel = context.WithTimeout(ctx, 150*time.Second)
+	// defer cancel()
+
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", false),
+	)
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	// create chrome instance
+	ctx, cancel := chromedp.NewContext(
+		allocCtx,
+		chromedp.WithLogf(log.Printf),
+	)
+	defer cancel()
+
 	tasks := []chromedp.Action{
 		chromedp.Navigate(`https://all.accor.com/usa/index.en.shtml`),
 		chromedp.DoubleClick(`#onetrust-close-btn-container > button`, chromedp.NodeVisible),
@@ -57,7 +77,7 @@ func main() {
 		chromedp.Sleep(5 * time.Second),
 		chromedp.WaitVisible(`#primary_button`),
 		chromedp.SetValue("#username-id", username),
-		chromedp.SetValue("#password-id", pass),
+		chromedp.SetValue("#password-id", password),
 		chromedp.Sleep(5 * time.Second),
 		chromedp.Click(`#primary_button`, chromedp.NodeVisible),
 		chromedp.Sleep(3 * time.Second),
@@ -71,35 +91,19 @@ func main() {
 		chromedp.Click(`#logout-button`, chromedp.NodeVisible),
 		chromedp.Sleep(3 * time.Second),
 	}
-	for i := 0; i < len(rows); i++ {
-		cells := rows[i]
-		username := cells[0]
-		pass := cells[1]
-		if i == 1 {
-			tasks = append(tasks[:1], tasks[2:]...)
-		}
 
-		if i == 0 {
-			tasks[8] = chromedp.SetValue("#username-id", (username))
-			tasks[9] = chromedp.SetValue("#password-id", pass)
-		} else {
-			tasks[7] = chromedp.SetValue("#username-id", username)
-			tasks[8] = chromedp.SetValue("#password-id", pass)
-		}
-
-		err := chromedp.Run(ctx, tasks...)
-		if err != nil {
-			fmt.Printf("err: %v\n", err)
-		} else {
-			WriteToLog(fmt.Sprintf("\nusername:%s,status:%s,points:%s", username, status, points))
-		}
-
+	err := chromedp.Run(ctx, tasks...)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	} else {
+		content := fmt.Sprintf("\nusername:%s,status:%s,points:%s", username, status, points)
+		fmt.Printf("content: %v\n", content)
+		WriteResult(content)
 	}
-
 }
 
-func WriteToLog(logContent string) {
-	logFilePath := time.Now().Format("20060102") + ".txt"
+func WriteResult(logContent string) {
+	logFilePath := "Result_" + time.Now().Format("20060102") + ".txt"
 	if _, err := os.Stat(logFilePath); os.IsNotExist(err) {
 		_, _ = os.Create(logFilePath)
 	}
